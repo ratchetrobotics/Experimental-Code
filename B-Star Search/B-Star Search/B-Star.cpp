c39551ac05aa670aa40f936f296adc7327c5fb5e
Vincent Bensch <vbensch@gmail.com>
2015-01-29 13:41:18 -0600

    pathlength function

--- B-Star Search/B-Star Search/B-Star.cpp
+++ B-Star Search/B-Star Search/B-Star.cpp
@@ -14,3 +14,12 @@ unsigned const int mapsizey = 140; // vertical size size of the map
 vector<pair<float, float>> path;
 pair < pair<float, float>, pair<float, float> >  directline;
 
+float pathlength(vector<pair<float, float>> tpath)
+{
+	float length;
+	for (int i = 1; i < tpath.size(); i++)
+	{
+		length += sqrt(pow(tpath[i].first - tpath[i - 1].first, 2) + pow(tpath[i].second - tpath[i - 1].second, 2));
+	}
+	return length;
+}
