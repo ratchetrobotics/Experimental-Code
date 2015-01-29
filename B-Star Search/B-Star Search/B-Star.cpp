2d12462dcc086825ca0ebe23572610199963cef6
Vincent Bensch <vbensch@gmail.com>
2015-01-29 13:31:04 -0600

    Variables Yo!

--- B-Star Search/B-Star Search/B-Star.cpp
+++ B-Star Search/B-Star Search/B-Star.cpp
@@ -7,3 +7,10 @@
 #include <vector>
 
 using namespace std;
+
+unsigned const int mapsizex = 140; // horizontal size of the map
+unsigned const int mapsizey = 140; // vertical size size of the map
+
+vector<pair<float, float>> path;
+pair < pair<float, float>, pair<float, float> >  directline;
+
